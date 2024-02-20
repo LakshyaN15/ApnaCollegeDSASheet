41b0d13bcdc024dc8b531abf255798808d78cca1
Lakshya Nagpal <lnagpal15@gmail.com>
2024-02-21 01:25:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (21.82%) - LeetSync

---
+++ LeetCode/Arrays/191-number-of-1-bits/number-of-1-bits.cpp
@@ -0,0 +1,16 @@
+// TIME COMPLEXITY: O(logn), the number of iterations required 
+// to reduce n to zero is proportional to the number of bits in n, 
+// which is log n
+// SPACE COMPLEXITY: O(1)
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+       int count=0;
+       while(n!=0){
+           if(n & 1) count++;
+           n=n>>1;
+       }
+       return count;
+    }
+};
